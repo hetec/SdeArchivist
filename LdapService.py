d06c97b543c06c5a20d9652c83b5dad675172b8b
hetec <hebner.tec@gmail.com>
2015-12-07 14:35:42 +0100

    Add doc string for ldap service

--- LdapService.py
+++ LdapService.py
@@ -1,15 +1,27 @@
+# -*- encoding utf-8 -*-
 import ldap
-import ldif
-import sys
 
 
 class LdapService:
+    """
+    Connects to the defined ldap server
+    """
 
     def __init__(self, properties):
+        """
+        Creates a LdapSever instance
+        :param properties: Properties to connect to the ldap server (Dictionary)
+        :return: New LdapService object
+        """
         self.__props = properties
         self.__ldapObj = ldap.initialize("ldap://" + self.__props["server"])
 
     def get_email_by_uid(self, uid):
+        """
+        Finds a email address by a given uid
+        :param uid: The ldap name of the user (String)
+        :return: Email (String)
+        """
         res = self.__ldapObj.search_s(self.__props["dn"],ldap.SCOPE_SUBTREE,"uid=" + uid)
         email = res[0][1]["mail"][0]
         return email
