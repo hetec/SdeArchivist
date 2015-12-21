f525921ffada5ec17ff2ae98289e5284eda4efb4
hetec <hebner.tec@gmail.com>
2015-12-21 13:32:05 +0100

    Add exception handling to the database connection object

--- OracleConnection.py
+++ OracleConnection.py
@@ -14,12 +14,22 @@ class OracleConnection:
     def __init__(self, properties):
         self.__db_props = properties
 
+    def set_console_logger(self, console_logger):
+        self.__c_logger = console_logger
+
+    def set_file_logger(self, file_logger):
+        self.__f_logger = file_logger
 
     def connection(self):
         """
         Returns a connection to the defined oracle database
         :return: cx_Oracle connection object
         """
-        url = cx_Oracle.makedsn(self.__db_props["url"], self.__db_props["port"], self.__db_props["service"])
-        connection = cx_Oracle.connect(self.__db_props["username"], self.__db_props["password"], url)
-        return connection
+        try:
+            url = cx_Oracle.makedsn(self.__db_props["url"], self.__db_props["port"], self.__db_props["service"])
+            connection = cx_Oracle.connect(self.__db_props["username"], self.__db_props["password"], url)
+            return connection
+        except Exception as e:
+            self.__c_logger.exception("Exception while connection to ORACLE: " + str(e))
+            self.__f_logger.exception("Exception while connection to ORACLE: " + str(e))
+            raise Exception("Exception while connection to ORACLE: " + str(e))
