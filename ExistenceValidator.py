a96f8c4c1cf9eb0770ac33655a3906be961be0c9
hetec <hebner.tec@gmail.com>
2015-12-21 13:17:04 +0100

    Add logging statements to existence validator

--- ExistenceValidator.py
+++ ExistenceValidator.py
@@ -12,8 +12,16 @@ class ExistenceValidator():
     def __init__(self,):
         pass
 
+    def set_console_logger(self, console_logger):
+            self.__c_logger = console_logger
+
+    def set_file_logger(self, file_logger):
+        self.__f_logger = file_logger
+
     def directory_exists(self, dir):
         path = os.path.abspath(dir)
+        self.__c_logger.debug("Dir exists: " + str(os.path.exists(path)))
+        self.__f_logger.debug("Dir exists: " + str(os.path.exists(path)))
         return os.path.exists(path)
 
     def buffered_xml_exists(self, file_name):
@@ -23,6 +31,8 @@ class ExistenceValidator():
         :return: (Boolean)
         """
         path = os.path.join(os.path.abspath("buffer"), file_name)
+        self.__c_logger.debug("XML exists: " + str(os.path.exists(path)))
+        self.__f_logger.debug("XML exists: " + str(os.path.exists(path)))
         return os.path.exists(path)
 
     def config_file_exists(self, file_name):
@@ -32,6 +42,8 @@ class ExistenceValidator():
         :return: (Boolean)
         """
         path = os.path.join(os.path.abspath("config"), file_name)
+        self.__c_logger.debug("Config exists: " + str(os.path.exists(path)))
+        self.__f_logger.debug("Config exists: " + str(os.path.exists(path)))
         return os.path.exists(path)
 
     def imported_sde_data_exists(self, connection_name, file_name):
@@ -43,6 +55,8 @@ class ExistenceValidator():
         if self.config_file_exists(connection_name + ".sde"):
             path = os.path.abspath("config/" + connection_name + ".sde/")
             arcpy.env.workspace = path
+            self.__c_logger.debug("Dir exists: " + str(arcpy.Exists(file_name)))
+            self.__f_logger.debug("Dir exists: " + str(arcpy.Exists(file_name)))
             return arcpy.Exists(file_name)
         else:
             raise ExistenceException("Missing connection file in the config folder")
