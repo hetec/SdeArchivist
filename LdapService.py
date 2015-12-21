513c0b48d14dd0ae20b02dd93f05c99e8cb1d8f9
hetec <hebner.tec@gmail.com>
2015-12-21 13:21:55 +0100

    Add logging statements to ldap service

--- LdapService.py
+++ LdapService.py
@@ -20,6 +20,12 @@ class LdapService:
         except Exception as e:
             raise Exception("Not able to connect to " + self.__props["server"] + ": " + str(e.message))
 
+    def set_console_logger(self, console_logger):
+            self.__c_logger = console_logger
+
+    def set_file_logger(self, file_logger):
+        self.__f_logger = file_logger
+
     def get_email_by_uid(self, uid):
         """
         Finds a email address by a given uid
@@ -29,12 +35,18 @@ class LdapService:
         """
         try:
             res = self.__ldapObj.search_s(self.__props["dn"],ldap.SCOPE_SUBTREE,"uid=" + uid)
+            self.__c_logger.debug("Get email address for " + uid +": " + res)
+            self.__f_logger.debug("Get email address for " + uid +": " + res)
         except Exception as e:
+            self.__c_logger.exception("EXCEPTION while getting email address for " + uid +": " + res + ": " + str(e))
+            self.__f_logger.exception("EXCEPTION while getting email address for " + uid +": " + res + ": " + str(e))
             raise Exception("Not able to execute the search request on the server: '" + self.__props["server"]
                             + "', ERROR: " + str(e.message))
         if len(res) > 0 and res is not None:
             email = res[0][1]["mail"][0]
         else:
+            self.__c_logger.exception("EXCEPTION while getting email address for " + uid + ": No ldap entry")
+            self.__f_logger.exception("EXCEPTION while getting email address for " + uid + ": No ldap entry")
             raise Exception("No ldap entry for '" + uid + "'")
         return email
 
