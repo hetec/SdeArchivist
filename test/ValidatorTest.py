08723b6e811b3064a4e6f6a13758b3c98fb647f1
hetec <hebner.tec@gmail.com>
2016-05-03 17:28:16 +0200

    Add unit tests for optional tags

--- test/ValidatorTest.py
+++ test/ValidatorTest.py
@@ -11,17 +11,19 @@ class ValidationTest(unittest.TestCase):
     c_logger = logging.getLogger("mock")
     f_logger = logging.getLogger("mock")
 
-    tag_not_empty_mapped = MetaDataTag("test1", False, "mapped1")
-    tag_not_empty_not_mapped = MetaDataTag("test2", False)
-    tag_empty_not_mapped = MetaDataTag("test3", True)
-    tag_empty_mapped = MetaDataTag("test4", True, "mapped4")
-    tag_empty_attribute = MetaDataTag("test5", True, mapped_name="", attributes=["attr51", "attr52"])
-    tag_not_empty_attribute = MetaDataTag("test6", False, mapped_name="", attributes=["attr6"])
-    unique_nest_tag_not_empty = MetaDataTag("unique_inner", False, mapped_name="")
-    multi_nest_tag_not_empty_1 = MetaDataTag("multi_inner", False, mapped_name="")
-    multi_nest_tag_not_empty_2 = MetaDataTag("multi_inner", False, mapped_name="")
-    multi_nest_tag_not_empty_path1 = MetaDataTag("test7/multi_inner", False, mapped_name="")
-    multi_nest_tag_not_empty_path2 = MetaDataTag("test8/multi_inner", False, mapped_name="")
+    tag_not_empty_mapped = MetaDataTag(tag_name="test1", is_empty=False, mapped_name="mapped1")
+    tag_not_empty_not_mapped = MetaDataTag(tag_name="test2", is_empty=False)
+    tag_empty_not_mapped = MetaDataTag(tag_name="test3", is_empty=True)
+    tag_empty_mapped = MetaDataTag(tag_name="test4", is_empty=True, mapped_name="mapped4")
+    tag_empty_attribute = MetaDataTag(tag_name="test5", is_empty=True, mapped_name="", attributes=["attr51", "attr52"])
+    tag_not_empty_attribute = MetaDataTag(tag_name="test6", is_empty=False, mapped_name="", attributes=["attr6"])
+    unique_nest_tag_not_empty = MetaDataTag(tag_name="unique_inner", is_empty=False, mapped_name="")
+    multi_nest_tag_not_empty_1 = MetaDataTag(tag_name="multi_inner", is_empty=False, mapped_name="")
+    multi_nest_tag_not_empty_2 = MetaDataTag(tag_name="multi_inner", is_empty=False, mapped_name="")
+    multi_nest_tag_not_empty_path1 = MetaDataTag(tag_name="test7/multi_inner", is_empty=False, mapped_name="")
+    multi_nest_tag_not_empty_path2 = MetaDataTag(tag_name="test8/multi_inner", is_empty=False, mapped_name="")
+    invalid_optional_tag = MetaDataTag(tag_name="test9", is_empty=False, mapped_name="optional", optional=True, attributes=["attr9"])
+    invalid_non_optional_tag = MetaDataTag(tag_name="test9", is_empty=False, mapped_name="non_optional", optional=False, attributes=["attr9"])
 
     x = '''
     <root>
@@ -38,6 +40,7 @@ class ValidationTest(unittest.TestCase):
         <test8>
             <multi_inner>Not empty</multi_inner>
         </test8>
+        <test9 attr9="attr9"></test9>
     </root>
     '''
 
@@ -117,7 +120,30 @@ class ValidationTest(unittest.TestCase):
         meta = MetaData()
         validator.validate(meta)
         self.assertEqual(meta.is_valid(), True, "Invalid tags")
-        #should_exist = [""]
+        if "test5_attr51" not in meta.meta_data() or "test5_attr52" not in meta.meta_data():
+            raise Exception("Wrong attributes")
+
+    def test_validate_optionalTag_doesntFailForInvalidTag(self):
+        tags = {"test9": self.invalid_optional_tag}
+        validator = MetaDataValidator(self.x, tags)
+        validator.set_console_logger(self.c_logger)
+        validator.set_file_logger(self.f_logger)
+
+        meta = MetaData()
+        validator.validate(meta)
+        self.assertEqual(meta.is_valid(), True, "Invalid tags")
+        if "optional_attr9" not in meta.meta_data():
+            raise Exception("Wrong attributes")
+
+    def test_validate_optionalTag_doesntFailForInvalidTag(self):
+        tags = {"test9": self.invalid_non_optional_tag}
+        validator = MetaDataValidator(self.x, tags)
+        validator.set_console_logger(self.c_logger)
+        validator.set_file_logger(self.f_logger)
+
+        meta = MetaData()
+        validator.validate(meta)
+        self.assertEqual(meta.is_valid(), False, "Valid tags")
 
 
 if __name__ == "__main__":
